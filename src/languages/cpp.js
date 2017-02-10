e8bf286404d00397c195cfae29f027cd20922946
samsam2310 <samsam2310@gmail.com>
2017-02-10 10:54:01 +0800

    move some cpp-typename from keyword to built-in

--- src/languages/cpp.js
+++ src/languages/cpp.js
@@ -66,16 +66,14 @@ function(hljs) {
   var FUNCTION_TITLE = hljs.IDENT_RE + '\\s*\\(';
 
   var CPP_KEYWORDS = {
-    keyword: 'int float while private char catch import module export virtual operator sizeof ' +
+    keyword: 'while private catch import module export virtual operator sizeof ' +
       'dynamic_cast|10 typedef const_cast|10 const struct for static_cast|10 union namespace ' +
-      'unsigned long volatile static protected bool template mutable if public friend ' +
-      'do goto auto void enum else break extern using class asm case typeid ' +
-      'short reinterpret_cast|10 default double register explicit signed typename try this ' +
+      'volatile static protected template mutable if public friend ' +
+      'do goto auto enum else break extern using class asm case typeid ' +
+      'reinterpret_cast|10 default register explicit typename try this ' +
       'switch continue inline delete alignof constexpr decltype ' +
-      'noexcept static_assert thread_local restrict _Bool complex _Complex _Imaginary ' +
-      'atomic_bool atomic_char atomic_schar ' +
-      'atomic_uchar atomic_short atomic_ushort atomic_int atomic_uint atomic_long atomic_ulong atomic_llong ' +
-      'atomic_ullong new throw return',
+      'noexcept static_assert thread_local restrict' +
+      'new throw return',
     built_in: 'std string cin cout cerr clog stdin stdout stderr stringstream istringstream ostringstream ' +
       'auto_ptr deque list queue stack vector map set bitset multiset multimap unordered_set ' +
       'unordered_map unordered_multiset unordered_multimap array shared_ptr abort abs acos ' +
@@ -84,7 +82,11 @@ function(hljs) {
       'isxdigit tolower toupper labs ldexp log10 log malloc realloc memchr memcmp memcpy memset modf pow ' +
       'printf putchar puts scanf sinh sin snprintf sprintf sqrt sscanf strcat strchr strcmp ' +
       'strcpy strcspn strlen strncat strncmp strncpy strpbrk strrchr strspn strstr tanh tan ' +
-      'vfprintf vprintf vsprintf endl initializer_list unique_ptr',
+      'vfprintf vprintf vsprintf endl initializer_list unique_ptr' +
+      // mv from keyword to built_in
+      'long int float char bool void double short signed unsigned _Bool _Complex _Imaginary complex ' +
+      'atomic_bool atomic_char atomic_schar atomic_ullong atomic_uchar atomic_short atomic_ushort ' +
+      ' atomic_intatomic_uint atomic_long atomic_ulong atomic_llong ',
     literal: 'true false nullptr NULL'
   };
 
